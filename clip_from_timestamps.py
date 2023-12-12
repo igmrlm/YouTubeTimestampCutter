55a08e773975dc0f6d8b10bbd0fd80c3e47c0e37
igmrlm <igmrlm@gmail.com>
2023-12-12 00:14:02 -0500

    Update clip_from_timestamps.py
    added numbering of files to keep them in the correct order when played back alphabetically.
    
    added a few print statements

--- clip_from_timestamps.py
+++ clip_from_timestamps.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import shlex
 import argparse
 
 # Function to convert timestamp to seconds
@@ -8,7 +9,7 @@ def timestamp_to_seconds(timestamp):
     return mm * 60 + ss
 
 def create_keyframe_database(video_file):
-    print("Creating keyframe database, this may take a while")
+    print("Creating Keyframe Database. This may take some time, please wait.")
     keyframe_database = {}
     ffprobe_command = f"ffprobe -i \"{video_file}\" -skip_frame nokey -select_streams v -show_entries frame=pkt_dts_time -of compact=p=0:nk=1 -v 0"
     try:
@@ -46,13 +47,24 @@ if not os.path.exists(output_directory):
 # Extract input file name without extension
 input_filename = os.path.splitext(os.path.basename(video_file))[0]
 
+print("Video Timestamp Extractor: This Python script helps extract video clips based on provided timestamps from a video file. It utilizes ffprobe and ffmpeg to locate keyframes and extract clips accurately.")
+print("https://github.com/igmrlm/YouTubeTimestampCutter")
+print("https://YouTube.com/NathanaelNewton")
+
+print("Loading video:")
+print(video_file)
+
+print("Loading timestamps from:")
+print(timestamp_file)
+
+
 # Create keyframe database
 keyframe_db = create_keyframe_database(video_file)
 
 # Extract clips based on timestamps
-for i in range(len(timestamps) - 1):
-    start_time_str, description = timestamps[i].split(' ', 1)
-    end_time_str, _ = timestamps[i + 1].split(' ', 1)
+for i, timestamp in enumerate(range(len(timestamps) - 1), start=1):
+    start_time_str, description = timestamps[timestamp].split(' ', 1)
+    end_time_str, _ = timestamps[timestamp + 1].split(' ', 1)
     
     start_time = timestamp_to_seconds(start_time_str)
     end_time = timestamp_to_seconds(end_time_str)
@@ -61,8 +73,8 @@ for i in range(len(timestamps) - 1):
     start_keyframe = find_nearest_keyframe(start_time, keyframe_db)
     end_keyframe = find_nearest_keyframe(end_time, keyframe_db)
     
-    # Use ffmpeg to extract the clip with input file name + timestamp description
-    output_filename = f"{output_directory}/{input_filename}_{description}.mp4"
+    # Use ffmpeg to extract the clip with input file name + numbering + timestamp description
+    output_filename = f"{output_directory}/{input_filename}_{i:03d}_{description}.mp4"
     start_keyframe = round(start_keyframe, 3)
     end_keyframe = round(end_keyframe, 3)
     
@@ -70,3 +82,4 @@ for i in range(len(timestamps) - 1):
     subprocess.call(ffmpeg_command, shell=True)
 
 print("Extraction completed.")
+print("Files are saved in .\output_clips"
